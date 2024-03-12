1211a3cbdea69b17d1126d09671c24b0996d421b
leonardopangaio <leonardo.pangaio@gmail.com>
2024-03-12 08:04:19 -0300

    block of 20240312

--- Exemplos/topico43-objetos.js
+++ Exemplos/topico43-objetos.js
@@ -1,3 +1,8 @@
+const res=document.querySelector(".res")
+const btn_add=document.querySelector("#btn_add")
+
+let carros = []
+
 class Carro{
     curso="Javascript"
     constructor(pNome, pTipo){
@@ -70,3 +75,25 @@ console.log(c1.getInfo())
 
 console.log("---")
 console.log(c1.curso)
+
+const addCarro=()=>{
+    res.innerHTML=""
+    carros.map((i)=>{
+        const div=document.createElement("div")
+        div.setAttribute("class","carro")
+        div.innerHTML=`Carro: ${i.getNome()}<br/>Tipo: ${i.getTipo()}`
+        res.appendChild(div)
+    })
+}
+
+btn_add.addEventListener("click",(evt)=>{
+    const f_carro=document.querySelector("#f_carro")
+    const f_tipo=document.querySelector("#f_tipo")
+    let c = new Carro(f_carro.value,f_tipo.value)
+    carros.push(c)
+    f_carro.value=""
+    f_tipo.value=""
+    f_carro.focus()
+    console.log(carros)
+    addCarro()
+})
